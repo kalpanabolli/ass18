5cbf32043b6fbc7d01acf393a27af55a391aed37
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-09-05 16:59:04 +0530

    first commit

---
+++ Ass18/Data/Ass18DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ass18.Models;
+
+namespace Ass18.Data
+{
+    public class Ass18DbContext : DbContext
+    {
+        public Ass18DbContext (DbContextOptions<Ass18DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ass18.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Ass18/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Ass18.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Ass18DbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Ass18DbContext") ?? throw new InvalidOperationException("Connection string 'Ass18DbContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
